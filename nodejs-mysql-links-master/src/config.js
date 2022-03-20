b43a295f95a5b31614bab0047a7f5efb6cbdc83d
awsecops <96880169+awsecops@users.noreply.github.com>
2022-03-20 21:24:47 +0530

    Update config.js

--- nodejs-mysql-links-master/src/config.js
+++ nodejs-mysql-links-master/src/config.js
@@ -7,9 +7,9 @@ config();
 export default {
   database: {
     connectionLimit: 10,
-    host: process.env.DATABASE_HOST || "localhost",
-    user: process.env.DATABASE_USER || "root",
-    password: process.env.DATABASE_PASSWORD || "faztpassword",
+    host: process.env.DATABASE_HOST || "cluster-eks.czojgjwy6aok.ap-south-1.rds.amazonaws.com",
+    user: process.env.DATABASE_USER || "eksrds",
+    password: process.env.DATABASE_PASSWORD || "Shiva1234",
     database: process.env.DATABASE_NAME || "dblinks",
   },
   port: process.env.PORT || 4000,
